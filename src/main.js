8496fcb6dd669b7a7d13b6d20eeb03566aaf5049
Danylo <tlink.hitech@gmail.com>
2026-06-14 15:55:19 +0700

    Update imports in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { getImagesByQuery } from './js/pixabay-api';
-import { imageGallery } from './js/render-functions';
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { imageGallery } from './js/render-functions.js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
